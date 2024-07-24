0e755332876caa6201e329a5da42070ec224a904
HANANEZOUBI <hananezoubi1@gmail.com>
2024-07-24 07:37:58 -0700

    validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+a method that determines if a given data
+set represents a valid UTF-8 encoding.
+"""
+
+from typing import List
+
+
+def validUTF8(data: List[int]) -> bool:
+    """
+    Returns true ifdata is valid UTF-8 encoding
+    Args: data - the list of ints to check
+    """
+    points = 0
+    bi = []
+
+    for byte in data:
+        bi.append(byte & 0xFF)
+
+    for byte in bi:
+
+        if points == 0:
+            if byte >> 3 == 0b11110:
+                points = 3
+
+            elif byte >> 4 == 0b1110:
+                points = 2
+
+            elif byte >> 5 == 0b110:
+                points = 1
+
+            elif byte >> 7 == 0:
+                points = 0
+
+            else:
+
+                return False
+        else:
+
+            if byte >> 6 != 0b10:
+                return False
+            points = points - 1
+
+    return points == 0
---
+++ 0x04-utf8_validation/README.md
@@ -0,0 +1,1 @@
+def validUTF8(data)
